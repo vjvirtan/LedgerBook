f11302908e29d9b46716f5b825f20b3cba588986
Ville Virtanen <Ville@nilkan.com>
2017-12-03 20:07:10 +0200

    removed folder

--- src/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ledgerbook.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.ledgerbook.config.UserDao;
-
-public interface UserRepository extends MongoRepository<UserDao, String> {
-	UserDao findByName(String name);
-}
